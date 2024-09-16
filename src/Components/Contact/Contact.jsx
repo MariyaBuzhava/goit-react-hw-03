7d15a0041bcf4a256017fc25f45d1005acfe97c6
Mariya Buzhava <bujava.masha@gmail.com>
2024-09-17 00:07:42 +0300

    add css

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,11 +1,15 @@
+import s from "./Contact.module.css";
+
 const Contact = ({ id, name, number, handleDeleteContact }) => {
   return (
-    <label htmlFor={id}>
-      <div>
+    <label htmlFor={id} className={s.lable}>
+      <div className={s.contact}>
         <span>{name}</span>
         <span>{number}</span>
       </div>
-      <button onClick={() => handleDeleteContact(id)}>Delete</button>
+      <button className={s.button} onClick={() => handleDeleteContact(id)}>
+        Delete
+      </button>
     </label>
   );
 };
